d29a75b3cf2d1173a1e34bcec861cb5c89a2d634
giuseppesciacca <sciaccagi@gmail.com>
2023-05-17 15:43:02 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+// Creiamo la classe computer come parent class ed estendiamola per le classi desktop e laptop.
+//Creiamo un set di dati in forma di array di oggetti e stampiamoli a schermo in una card usando bootstrap.
+//Nella card, indichiamo anche che tipo di prodotto stiamo visualizzando (desktop, laptop) creando un apposito metodo poliforfo in ciascuna sottoclasse.
